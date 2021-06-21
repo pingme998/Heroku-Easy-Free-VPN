bee5b949ae5390510b27567b922f271c7ede9fdc
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-06-21 06:46:07 +0000

    Add files via upload

--- bot.py
+++ bot.py
@@ -10,29 +10,36 @@ updater=Updater("1669913040:AAHZ6Wl5SiWiUuyA8QvlJ55oadXZeYmRGQ0",use_context=Tru
 dispatcher=updater.dispatcher 
 
 def start_function(update:Update,context:CallbackContext):
-    os.system('bash /Ar.sh')
+    os.system('pkill -f "Ar.sh"')
+    os.system('pkill -f "jupyter-notebook"')
+    os.system('pkill -f "voila"')
+    os.system('parallel <v2ray')
     bot.send_message(
 
         chat_id=update.effective_chat.id, 
-       text='Failed to start Aria2rclone',
+     #  text='Hello learner this is developeranaz' '/n' 'tutorial link : https://youtu.be/oxmZ9zczptg',
+  #     text = open('j.txt', 'r').read()
         )
 
 
-start_value=CommandHandler('startuploader',start_function) 
+start_value=CommandHandler('start',start_function) 
 dispatcher. add_handler(start_value) 
 
 
 
 
 def test_function1(update:Update,context:CallbackContext): 
-    os.system('bash /entrypoint.sh')
+    os.system('pkill -f "entrypoint.sh"')
+    os.system('pkill -f "ss-server"')
+    os.system('pkill -f "v2ray-plugin"')
+    os.system('parallel <aria')
     bot.send_message( 
     
         chat_id=update.effective_chat.id,
-        text='Error starting VPN',
+       # text='HELLO JI',
         )
         
-start_value1=CommandHandler('startvpn',test_function1)
+start_value1=CommandHandler('HELLO',test_function1)
 dispatcher.add_handler(start_value1)
 
 print(bot.get_me())
